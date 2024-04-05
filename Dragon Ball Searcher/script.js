368867c8b703faaf1ed333c25329c1f3befd2839
Mirko <miroslav.magerov.7e5@itb.cat>
2024-04-05 12:13:19 +0200

    Intento de cambiar la app de vue a un script separado

---
+++ Dragon Ball Searcher/script.js
@@ -0,0 +1,64 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            search: "",
+            dbChar: "",
+            searchResult: null,
+            favs: new Map()
+        }
+    },
+    computed: {
+        favsArray() {
+            return Array.from(this.favs.values());
+        }
+    },
+    methods: {
+        buscarDragonBallCharacter() {
+            fetch(`https://dragonball-api.com/api/characters?name=${this.search}`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.length > 0) {
+                        this.dbChar = data[0];
+                        this.searchResult = true;
+                    } else {
+                        this.searchResult = false;
+                    }
+                })
+
+                .catch(error => {
+                    console.error('Error:', error);
+                    this.searchResult = null;
+                });
+        },
+        addToFavorite() {
+            this.favs.set(this.dbChar.name, this.dbChar);
+            this.saveFavs();
+        },
+        saveFavs() {
+            localStorage.setItem("favs", JSON.stringify(this.favsArray));
+        },
+        showFavorites() {
+            const favsData = JSON.parse(localStorage.getItem("favs"));
+
+            if (favsData) {
+                this.favs = new Map(favsData);
+            }
+        },
+        deleteFavs() {
+            localStorage.clear();
+            this.favs.clear();
+        },
+        
+    },
+    mounted() {
+        let newFavs = localStorage.getItem("favs");
+
+        if (newFavs) {
+            for (let fav of JSON.parse(newFavs)) {
+                this.favs.set(fav.name, fav);
+            }
+        }
+    }
+})
+
+app.mount('#app');
